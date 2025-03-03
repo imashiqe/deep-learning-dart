8c7c32d32810255c799d7422443763162447ee37
Ashiqur Rahman <imashiqe@gmail.com>
2025-03-03 23:12:11 +0600

    simple calculation

---
+++ module02/variable.dart
@@ -0,0 +1,13 @@
+main() {
+  int number1 = 10;
+  int number2 = 20;
+  String number3 = "25";
+
+  print("Sum: ${number1 + number2}");
+  print("Difference: ${number1 - number2}");
+  print("Product: ${number1 * number2}");
+  print("Quotient: ${number1 / number2}");
+  print("Remainder: ${number1 % number2}");
+  print("Parse: ${int.parse(number3)}");
+  print("Double: ${double.parse(number3)}");
+}
