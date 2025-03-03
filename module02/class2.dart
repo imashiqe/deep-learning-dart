c150c8ee354f4f76f33abcd33545013c35447aca
Ashiqur Rahman <imashiqe@gmail.com>
2025-03-03 22:47:14 +0600

    data type

---
+++ module02/class2.dart
@@ -0,0 +1,15 @@
+main() {
+  int numberOne = 5;
+  String name = "Taufiq";
+  bool isTrue = false;
+  double numberTwo = 5.5;
+  print(numberOne);
+
+  String firstName = "Taufiq";
+  String lastName = "Hidayat";
+  String fullName = firstName + " " + lastName;
+  print(fullName);
+
+  print(firstName.toUpperCase());
+  print(firstName.toLowerCase());
+}
